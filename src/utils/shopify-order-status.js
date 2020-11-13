33d73350f9845d57542af726809e190b6fe4386e
Jim Toth <jim@meme.coach>
2020-11-13 08:47:25 -0500

    Updates funding-status endpoint to count total number of shopify paid shopify orders

--- src/utils/shopify-order-status.js
+++ src/utils/shopify-order-status.js
@@ -1,66 +1,37 @@
 const Hoek = require('@hapi/hoek')
+const debug = require('debug')('shopify-order-status')
 const Shopify = require('shopify-api-node')
-const {JSONPath} = require('jsonpath-plus')
 
-
-const getStatus = async()=>{
-
-  if(
-    ! process.env.SHOPIFY_SHOP_NAME ||
-    ! process.env.SHOPIFY_APIKEY ||
-    ! process.env.SHOPIFY_PASSWORD
-  )
-  {
-    console.log('shopify not enabled')
-    return {
-      raised: 0,
-      units: 0
+module.exports = async () => {
+  let units = 0
+  try {
+    if (
+      !process.env.SHOPIFY_SHOP_NAME ||
+      !process.env.SHOPIFY_APIKEY ||
+      !process.env.SHOPIFY_PASSWORD
+    ) {
+      throw new Error('shopify not enabled')
     }
-  }
 
-  const shopify = new Shopify({
-    shopName: process.env.SHOPIFY_SHOP_NAME,
-    apiKey: process.env.SHOPIFY_APIKEY,
-    password: process.env.SHOPIFY_PASSWORD,
-    apiVersion: '2019-10'
-  })
-
-  const orderCount = await shopify.order.count()
-
-  let orderList = []
-  let queryParam = {limit: 100}
-
-  while(orderList.length < orderCount){
-    const orders = await shopify.order.list(queryParam)
-
-
-    queryParam.page_info = Hoek.reach(orders, 'nextPageParameters.page_info')
-
-    orders.map( order => {
-      orderList.push(order)
+    const shopify = new Shopify({
+      shopName: process.env.SHOPIFY_SHOP_NAME,
+      apiKey: process.env.SHOPIFY_APIKEY,
+      password: process.env.SHOPIFY_PASSWORD,
+      apiVersion: '2020-10'
     })
 
-    if(!queryParam.page_info){ break }
-  }
-  
-
-  const priceQuery = `$..line_items[?(@.title === "Pocket P.C." || @.title === "Pocket P.C. w/ LoRa")].price`
-  const prices = JSONPath(priceQuery, orderList)
-
-  let sum = 0
-  prices.map( price => {
-    let parsed = parseFloat(price)
-    sum += parsed
-    return parsed
+    // TODO -> Verify which orders should be counted
+    const orderCount = await shopify.order.count({
+      status: 'any',
+      financial_status: 'paid'
     })
+    debug('Got paid order count', orderCount)
+    units = orderCount
 
-  console.log ('units', prices.length)
-  console.log ('raised', sum)
-  
-  return {
-    raised: sum,
-    units: prices.length
+    // TODO -> count # of units instead of # of orders?
+  } catch (e) {
+    debug('ERROR', e)
+  } finally {
+    return { units }
   }
 }
-
-module.exports = getStatus
